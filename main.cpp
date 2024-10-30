0fb34c6db4ad89588e3708a1ac3efd236bae5803
giosar-08 <giorgio.saracino@marconibari.edu.it>
2024-10-30 15:57:48 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
+
 #include <iostream>
-using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int N, vmin, vmax, s, i;
+    s=0
+    i=0
+    vmax=0
+    vmin=0
+    do while (N<0 or N>0)
+    {
+     i=i+1
+     cout::<< "numero";
+     cin::>> N;
+     if (N>0)
+       true
+       if (N>vmax)
+       true
+       vmax=N
+       false
+       if (N<vmin)
+       true 
+       vmin=N
+       s=s+1
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    }
+    cout::<<"il valore minimo è " & vmin
+    cout::<<"il valore massimo è " & vmax
+    cout::<<"numeri negativi inseriti " & s
+
+
+{
+  
